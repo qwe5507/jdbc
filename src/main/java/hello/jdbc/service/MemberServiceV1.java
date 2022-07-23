4c6dc912f9242737abdba4480fa793af41bbdd50
qwe5507 <apvmffkdls@gmail.com>
2022-07-24 00:04:44 +0900

    예외 오타 수정

--- src/main/java/hello/jdbc/service/MemberServiceV1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -21,7 +21,7 @@ public class MemberServiceV1 {
 
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
-            throw new IllegalArgumentException("이체중 예외 발생");
+            throw new IllegalStateException("이체중 예외 발생");
         }
     }
 }
